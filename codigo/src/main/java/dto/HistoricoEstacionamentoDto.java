d1ab0d1c53fbcdf01d8e8c3f6daefe4061d2815f
Sofia Vasconcelos Moreira e Silva <sofiavasconcelosmsilva@gmail.com>
2024-12-08 18:16:07 -0300

    Update HistoricoEstacionamentoDto.java

--- codigo/src/main/java/dto/HistoricoEstacionamentoDto.java
+++ codigo/src/main/java/dto/HistoricoEstacionamentoDto.java
@@ -1,52 +1,52 @@
-package main;
+package dto;
 
-import java.util.Scanner;
+import java.time.LocalDateTime;
 
-import controller.ClienteController;
-import controller.CobrancaController;
-import controller.ParqueEstacionamentoController;
-import view.ClienteView;
-import view.EstacionamentoView;
+public class HistoricoEstacionamentoDto {
+    private Long id;
+    private String identificadorVaga;
+    private String cpfCliente;
+    private LocalDateTime dataEntrada;
+    private LocalDateTime dataSaida;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner leitor = new Scanner(System.in);
-        // Inicializa o Parque de Estacionamento e Controladores
-        ParqueEstacionamentoController estacionamentoController = new ParqueEstacionamentoController(1, 10);
-        ClienteController clienteController = new ClienteController(estacionamentoController.getParqueEstacionamento(), leitor);
-        CobrancaController cobrancaController = new CobrancaController(estacionamentoController.getParqueEstacionamento(), leitor);
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
 
-        // Inicializa as views
-        ClienteView clienteView = new ClienteView(estacionamentoController.getParqueEstacionamento(), clienteController);
-        EstacionamentoView estacionamentoView = new EstacionamentoView(estacionamentoController);
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-        int opcao = 0; // Inicializa a variável opcao
-        do {
-            System.out.println("=======================================");
-            System.out.println("            SEJA BEM VINDO AO          ");
-            System.out.println("          PARQUE ESTACIONAMENTO        ");
-            System.out.println("=======================================");
-            System.out.println("          Digite uma opção             ");
-            System.out.println("(1) Área do Cliente");
-            System.out.println("(2) Área do Estacionamento");
-            System.out.println("(3) Sair");
-            System.out.print("Escolha uma opção: ");
-            opcao = leitor.nextInt();
-            leitor.nextLine(); // Consumir quebra de linha
+    public String getIdentificadorVaga() {
+        return identificadorVaga;
+    }
 
-            switch (opcao) {
-                case 1:
-                    clienteView.exibirMenu();
-                    break;
-                case 2:
-                    estacionamentoView.exibirMenu();
-                    break;
-                case 3:
-                    System.out.println("Saindo...");
-                    break;
-                default:
-                    System.out.println("Opção inválida.");
-            }
-        } while (opcao != 4);
+    public void setIdentificadorVaga(String identificadorVaga) {
+        this.identificadorVaga = identificadorVaga;
+    }
+
+    public String getCpfCliente() {
+        return cpfCliente;
+    }
+
+    public void setCpfCliente(String cpfCliente) {
+        this.cpfCliente = cpfCliente;
+    }
+
+    public LocalDateTime getDataEntrada() {
+        return dataEntrada;
+    }
+
+    public void setDataEntrada(LocalDateTime dataEntrada) {
+        this.dataEntrada = dataEntrada;
+    }
+
+    public LocalDateTime getDataSaida() {
+        return dataSaida;
+    }
+
+    public void setDataSaida(LocalDateTime dataSaida) {
+        this.dataSaida = dataSaida;
     }
 }
