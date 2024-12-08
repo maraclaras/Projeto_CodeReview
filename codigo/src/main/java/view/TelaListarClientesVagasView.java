23c0ed908407bde95245b67aeb3f3a7f84208955
Sofia Vasconcelos Moreira e Silva <sofiavasconcelosmsilva@gmail.com>
2024-12-08 18:27:38 -0300

    Update TelaListarClientesVagasView.java

--- codigo/src/main/java/view/TelaListarClientesVagasView.java
+++ codigo/src/main/java/view/TelaListarClientesVagasView.java
@@ -1,8 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
 package view;
+
+import modal.Cliente;
+import modal.Vaga;
+import modal.Veiculo;
+import modal.ParqueEstacionamento;
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TelaListarClientesVagasView extends javax.swing.JFrame {
 
@@ -11,6 +15,7 @@ public class TelaListarClientesVagasView extends javax.swing.JFrame {
      */
     public TelaListarClientesVagasView() {
         initComponents();
+        preencherListaClientesEVagas(); // Preenche o JList com as informações ao abrir a tela
     }
 
     /**
@@ -22,25 +27,10 @@ public class TelaListarClientesVagasView extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jScrollPane2 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
-        jLabel1 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
         jList1 = new javax.swing.JList<>();
+        jLabel1 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
-
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
-            new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
-            },
-            new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
-            }
-        ));
-        jScrollPane2.setViewportView(jTable1);
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -92,8 +82,48 @@ public class TelaListarClientesVagasView extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton1ActionPerformed
+        TelaEstacionamentoView telaCliente = new TelaEstacionamentoView();
+        telaCliente.setVisible(true);
+        this.setVisible(false);
+    }
+
+    // Método para listar os clientes e suas vagas associadas
+    private String[] listarClientesEVagas() {
+        // Criar uma lista para armazenar as informações de clientes e suas vagas
+        List<String> listaClientesVagas = new ArrayList<>();
+
+        // Iterar pelos clientes e associar a vaga
+        for (Cliente cliente : ParqueEstacionamento.clientes) {
+            String clienteNome = cliente.getNome();
+            // Procurar se o cliente tem alguma vaga associada
+            Vaga vagaAssociada = null;
+            for (Veiculo veiculo : cliente.listarVeiculos()) {
+                vagaAssociada = ParqueEstacionamento.getInstancia(0, 0).obterVagaPorVeiculo(veiculo);
+                if (vagaAssociada != null) {
+                    break;
+                }
+            }
+            
+            // Caso o cliente tenha uma vaga associada, adicione à lista
+            if (vagaAssociada != null) {
+                listaClientesVagas.add(clienteNome + " - Vaga: " + vagaAssociada.getIdentificador());
+            } else {
+                listaClientesVagas.add(clienteNome + " - Vaga: Nenhuma");
+            }
+        }
+
+        // Converter a lista para um array de Strings
+        return listaClientesVagas.toArray(new String[0]);
+    }
+
+    // Preencher o JList com as informações de clientes e suas vagas
+    private void preencherListaClientesEVagas() {
+        // Obter as informações de clientes e suas vagas
+        String[] clientesEVagas = listarClientesEVagas();
+        
+        // Adicionar as informações ao JList
+        jList1.setListData(clientesEVagas);
+    }
 
     /**
      * @param args the command line arguments
@@ -101,9 +131,6 @@ public class TelaListarClientesVagasView extends javax.swing.JFrame {
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
@@ -121,7 +148,6 @@ public class TelaListarClientesVagasView extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(TelaListarClientesVagasView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
-        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
@@ -136,7 +162,5 @@ public class TelaListarClientesVagasView extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JList<String> jList1;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JTable jTable1;
     // End of variables declaration//GEN-END:variables
 }
