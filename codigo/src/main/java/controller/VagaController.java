465b0aaee06d87536d97e3e8af0c04f07552bacd
Luisa Silva <luisaclara04@gmail.com>
2025-04-20 18:05:19 -0300

    Com novo DTO, controller foi arrumado para que chame ele e classe antiga veiculo passa a ser excluida

--- codigo/src/main/java/controller/VagaController.java
+++ codigo/src/main/java/controller/VagaController.java
@@ -1,36 +1,47 @@
 package controller;
 
-import DAO.Vaga;
-import DAO.VagaDAO;
+import DTO.VagaDTO;
 import exceptions.VagaInvalidaException;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class VagaController {
-    
-    public VagaDAO criarVaga(String identificador, int maxVagasPermitidas) {
-        try {
-            Vaga vaga = new Vaga(identificador, maxVagasPermitidas);
-            return new VagaDAO(vaga.getIdentificador(), vaga.isOcupada());
-        } catch (VagaInvalidaException e) {
-            System.out.println(e.getMessage());
-            return null;
+    private static final int MAX_VAGAS = 100; // Limite máximo de vagas
+    private static int vagasCriadas = 0;
+    private List<VagaDTO> vagas = new ArrayList<>();
+
+    public VagaDTO criarVaga(String identificador, boolean pcd) throws VagaInvalidaException {
+        if (vagasCriadas >= MAX_VAGAS) {
+            throw new VagaInvalidaException("Limite máximo de vagas atingido.");
         }
-    }
 
-    public void ocuparVaga(Vaga vaga) {
-        try {
-            vaga.ocupar();
-            System.out.println("A vaga " + vaga.getIdentificador() + " foi ocupada.");
-        } catch (VagaInvalidaException e) {
-            System.out.println(e.getMessage());
+        if (!validarIdentificador(identificador)) {
+            throw new VagaInvalidaException("Identificador de vaga inválido: " + identificador);
         }
+
+        VagaDTO vaga = new VagaDTO(identificador, pcd);
+        vagas.add(vaga);
+        vagasCriadas++;
+        return vaga;
     }
 
-    public void liberarVaga(Vaga vaga) {
-        vaga.liberar();
-        System.out.println("A vaga " + vaga.getIdentificador() + " foi liberada.");
+    public void ocuparVaga(VagaDTO vaga) throws VagaInvalidaException {
+        if (vaga.isOcupada()) {
+            throw new VagaInvalidaException("A vaga " + vaga.getIdentificador() + " já está ocupada.");
+        }
+        vaga.setOcupada(true);
     }
 
-    public VagaDAO obterVagaDAO(Vaga vaga) {
-        return new VagaDAO(vaga.getIdentificador(), vaga.isOcupada());
+    public void liberarVaga(VagaDTO vaga) {
+        vaga.setOcupada(false);
+    }
+
+    public List<VagaDTO> listarVagas() {
+        return new ArrayList<>(vagas);
+    }
+
+    private boolean validarIdentificador(String identificador) {
+        return identificador.matches("[A-Z][0-9]{2}");
     }
 }
