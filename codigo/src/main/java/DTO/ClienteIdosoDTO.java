b80882d255dae6d18627982c8026e24cf65187b8
Luisa Silva <luisaclara04@gmail.com>
2025-04-20 18:15:23 -0300

    Idosos e vip deixam de ser classes em dao e passam a ser classes dto que implementa a interface de cliente

---
+++ codigo/src/main/java/DTO/ClienteIdosoDTO.java
@@ -0,0 +1,46 @@
+package DTO;
+
+public class ClienteIdosoDTO implements ClienteDTO {
+    private Long id;
+    private String name;
+    private String email;
+    private boolean espacoSuficiente;
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isEspacoSuficiente() {
+        return espacoSuficiente;
+    }
+
+    public void setEspacoSuficiente(boolean espacoSuficiente) {
+        this.espacoSuficiente = espacoSuficiente;
+    }
+}
